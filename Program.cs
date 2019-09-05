f74e17aaebc5c907eddd77ad5b80e76c72aaeaf7
Joseph <Joseph@LENOVO-PC>
2019-09-05 08:31:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Name: Joe Carusillo
+//Date: 9/5/2019
+//Proect: Hello World/GitHub
+//Description: Create a Hellow world project then publish it to GitHub account
+using System;
+
+namespace _4300_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //writes a line of code
+            Console.WriteLine("Press any key to continue...");
+
+            //Creates a space 
+            Console.WriteLine("");
+
+            //writes a line saying Hello World!
+           Console.WriteLine("Hello World!");
+
+            //Keeps the window up so you can read it
+           Console.ReadKey(true);
+        }
+    }
+}
